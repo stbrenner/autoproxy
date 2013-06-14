6fb3378ccb649ae7accbd915c195b19630552e80
Stephan Brenner <ymx@server.fake>
2013-06-14 23:53:21 +0200

    Refactored + VPN handling

--- logic-windows.c
+++ logic-windows.c
@@ -15,26 +15,26 @@
 /**
  * Writes a single proxy setting to the batch file
  */
-void write_proxy_to_batch_file(FILE* file, const char* protocol, const char* proxy)
+void write_proxy_to_batch_file(FILE* file, const proxy_config* config, const int pos)
 {
-  wchar_t* protocolW;
-  wchar_t* proxyW;
-  int size;
+  wchar_t* protocolW = 0;
+  wchar_t* proxyW    = 0;
+  int size           = 0;
 
-  if (!proxy)
+  if (!(*config)[pos])
   {
-    wprintf(L"%6s -> no proxy\n", protocol);
-    fwprintf(file, L"set %s_proxy=\n", protocol);
+    printf("%6s -> no proxy\n", TARGET_PROTOCOLS[pos]);
+    fprintf(file, "set %s_proxy=\n", TARGET_PROTOCOLS[pos]);
     return;
   }
 
-  size = MultiByteToWideChar(CP_UTF8, 0, proxy, -1, 0, 0);
+  size = MultiByteToWideChar(CP_UTF8, 0, (*config)[pos], -1, 0, 0);
   proxyW = (wchar_t*)malloc(size * sizeof(wchar_t));
-  MultiByteToWideChar(CP_UTF8, 0, proxy, -1, proxyW, size);
+  MultiByteToWideChar(CP_UTF8, 0, (*config)[pos], -1, proxyW, size);
 
-  size = MultiByteToWideChar(CP_UTF8, 0, protocol, -1, 0, 0);
+  size = MultiByteToWideChar(CP_UTF8, 0, TARGET_PROTOCOLS[pos], -1, 0, 0);
   protocolW = (wchar_t*)malloc(size * sizeof(wchar_t));
-  MultiByteToWideChar(CP_UTF8, 0, protocol, -1, protocolW, size);
+  MultiByteToWideChar(CP_UTF8, 0, TARGET_PROTOCOLS[pos], -1, protocolW, size);
 
   wprintf(L"%6s -> %s\n", protocolW, proxyW);
   fwprintf(file, L"set %s_proxy=%s\n", protocolW, proxyW);
@@ -66,10 +66,9 @@ int write_proxy_config(const proxy_config* config)
   wprintf(L"Proxy configuration successfully detected:\n");
   fwprintf(file, L"@echo off\n");
 
-  for (i = 0;; i += 2)
+  for (i = 0; i < PROXY_ARRAY_LEN; i++)
   {
-    if (!(*config)[i]) break;
-    write_proxy_to_batch_file(file, (*config)[i], (*config)[i + 1]);
+    write_proxy_to_batch_file(file, config, i);
   }
 
   if (fclose(file))
@@ -84,14 +83,15 @@ int write_proxy_config(const proxy_config* config)
 /**
  * Detect the proxy for a specific protocol
  */
-void detect_proxy(HINTERNET http_session, const wchar_t* test_url, char** proxy_url, const wchar_t* protocol)
+void detect_proxy(HINTERNET http_session, proxy_config* config, int pos)
 {
   wchar_t*                  firstProxy        = 0;
   wchar_t*                  nextProxy         = 0;
   WINHTTP_PROXY_INFO        proxyInfo;
   WINHTTP_AUTOPROXY_OPTIONS autoProxyOptions;
-  int                       convertedSize     = 0;
-  wchar_t*                  proxyUrlW          = 0;
+  int                       size              = 0;
+  wchar_t*                  proxyUrlW         = 0;
+  wchar_t*                  testUrlW          = 0;
 
   ZeroMemory(&proxyInfo, sizeof(proxyInfo));
   ZeroMemory(&autoProxyOptions, sizeof(autoProxyOptions));
@@ -100,27 +100,35 @@ void detect_proxy(HINTERNET http_session, const wchar_t* test_url, char** proxy_
   autoProxyOptions.dwAutoDetectFlags = WINHTTP_AUTO_DETECT_TYPE_DHCP | WINHTTP_AUTO_DETECT_TYPE_DNS_A;
   autoProxyOptions.fAutoLogonIfChallenged = TRUE;
 
-  if(!WinHttpGetProxyForUrl(http_session, test_url, &autoProxyOptions, &proxyInfo))
+  size = MultiByteToWideChar(CP_UTF8, 0, PROXY_TEST_URLS[pos], -1, 0, 0);
+  testUrlW = (wchar_t*)malloc(size * sizeof(wchar_t));
+  MultiByteToWideChar(CP_UTF8, 0, PROXY_TEST_URLS[pos], -1, testUrlW, size);
+
+  if(!WinHttpGetProxyForUrl(http_session, testUrlW, &autoProxyOptions, &proxyInfo) || 
+     !proxyInfo.lpszProxy)
   {
-    *proxy_url = 0;   // No proxy detected
-    return;
+    return;   // No proxy detected
   }
 
   firstProxy = wcstok_s(proxyInfo.lpszProxy, L";", &nextProxy);
   proxyUrlW = (wchar_t*)malloc((wcslen(firstProxy) + 10) * sizeof(wchar_t));
-  wsprintf(proxyUrlW, L"%s://%s", protocol, firstProxy); 
+  wsprintf(proxyUrlW, L"%s://%s", PROXY_PROTOCOLS[pos], firstProxy); 
 
-  convertedSize = WideCharToMultiByte(CP_UTF8, 0, proxyUrlW, -1, 0, 0, 0, 0);
-  *proxy_url = (char*)malloc(convertedSize * sizeof(char));
-  WideCharToMultiByte(CP_UTF8, 0, proxyUrlW, -1, *proxy_url, convertedSize, 0, 0);
+  size = WideCharToMultiByte(CP_UTF8, 0, proxyUrlW, -1, 0, 0, 0, 0);
+  (*config)[pos] = (char*)malloc(size * sizeof(char));
+  WideCharToMultiByte(CP_UTF8, 0, proxyUrlW, -1, (*config)[pos], size, 0, 0);
 }
 
 /**
  * Automatically detect proxies for the various protocols
  */
-int detect_proxy_config(const proxy_config* config)
+int detect_proxy_config(proxy_config* config)
 {
   HINTERNET httpSession = NULL;
+  int       i           = 0;
+
+  *config = (proxy_config)malloc(PROXY_ARRAY_LEN * sizeof(char*));
+  memset(*config, 0, PROXY_ARRAY_LEN * sizeof(char*));
 
   httpSession = WinHttpOpen(L"autoproxy/1.0", WINHTTP_ACCESS_TYPE_NO_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0);
   if(!httpSession)
@@ -129,9 +137,10 @@ int detect_proxy_config(const proxy_config* config)
     return 1;
   }
 
-  detect_proxy(httpSession, L"http://www.google.com/",  &(*config)[1], L"http");
-  detect_proxy(httpSession, L"https://www.google.com/", &(*config)[3], L"http");
-  detect_proxy(httpSession, L"ftp://ftp.mozilla.org/",  &(*config)[5], L"ftp");
+  for (i = 0; i < PROXY_ARRAY_LEN; i++)
+  {
+    detect_proxy(httpSession, config, i);
+  } 
 
   WinHttpCloseHandle(httpSession);
   return 0;
